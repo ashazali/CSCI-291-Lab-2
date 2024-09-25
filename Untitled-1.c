4d3aabbbfb1416169ff1043798d7a7f1ecfe02e6
ashazali <ashazali10@gmail.com>
2024-09-25 12:26:43 +0400

    Add files via upload

---
+++ Untitled-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math>
+
+#define ESPRESSO_BEANS 8
+#define ESPRESSO_WATER 30
+#define ESPRESSO_PRICE 3.50
+#define CAPPUCCINO_BEANS 8
+#define CAPPUCCINO_WATER 30
+#define CAPPUCCINO_MILK 70
+#define CAPPUCCINO_PRICE 4.5
+#define MOCHA_BEANS 8
+#define MOCHA_WATER 39
+#define MOCHA_MILK 160
+#define MOCHA_CHOCOLATESYRUP 30
+#define MOCHA_PRICE 5.5
+
+
